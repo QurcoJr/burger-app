da6d3d52b1688b6e7c502aa739bc53f3f10132c3
QurcoJr <60933748+QurcoJr@users.noreply.github.com>
2020-08-16 17:41:48 +0400

    improving performance

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,19 +3,24 @@ import React from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.module.css';
 
-const Modal = props => (
-  <>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </>
-);
+const Modal = props => {
+  return (
+    <>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0'
+        }}>
+        {props.children}
+      </div>
+    </>
+  );
+};
 
-export default Modal;
+const areEqual = (prevProps, nextProps) => {
+  return nextProps.show === prevProps.show;
+};
+
+export default React.memo(Modal, areEqual);
