38db13018464e50b2dd491a49d45daa1d9dc0e6f
QurcoJr <60933748+QurcoJr@users.noreply.github.com>
2020-08-16 17:40:51 +0400

    add burger menu

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,16 +3,24 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const SideDrawer = props => {
-  // ...
+  const attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses[1] = classes.Open;
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <Logo height="11%" />
-      <nav style={{ marginTop: '32px' }}>
-        <NavigationItems />
-      </nav>
-    </div>
+    <>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <Logo height="11%" />
+        <nav style={{ marginTop: '32px' }}>
+          <NavigationItems />
+        </nav>
+      </div>
+    </>
   );
 };
 
